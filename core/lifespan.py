d885f367c2182caf76af4d82d43060df47152754
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-28 19:00:27 +0100

    Update lifespan.py

--- core/lifespan.py
+++ core/lifespan.py
@@ -7,7 +7,7 @@ from core.logging_config import logger
 async def lifespan(app):
 
     logger.info("=" * 60)
-    logger.info("Starting %s", app.title)
+    logger.info("%s starting...", app.title)
     logger.info("Environment: %s", app.debug)
     logger.info("Application ready.")
     logger.info("=" * 60)
@@ -15,6 +15,6 @@ async def lifespan(app):
     yield
 
     logger.info("=" * 60)
-    logger.info("Stopping %s", app.title)
+    logger.info("%s shutting down...", app.title)
     logger.info("Shutdown complete.")
     logger.info("=" * 60)
