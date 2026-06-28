02c7d7fc5e837d16ced0967c521675f5d181be6b
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-28 18:12:44 +0100

    Create health.py

---
+++ routes/health.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from core.config import settings
+
+router = APIRouter(
+    prefix="/health",
+    tags=["Health"]
+)
+
+
+@router.get("/")
+def health():
+
+    return {
+        "status": "healthy",
+        "service": settings.APP_NAME,
+        "version": settings.APP_VERSION,
+        "environment": settings.ENVIRONMENT
+    }
