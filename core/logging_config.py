ffa892d6c37d0f0e01dd65bf63adeb2dd6573914
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-28 18:45:26 +0100

    Update logging_config.py

--- core/logging_config.py
+++ core/logging_config.py
@@ -4,12 +4,19 @@ import sys
 from core.config import settings
 
 
+LOG_FORMAT = (
+    "%(asctime)s | %(levelname)-8s | "
+    "%(name)s | %(message)s"
+)
+
+
 logging.basicConfig(
     level=getattr(logging, settings.LOG_LEVEL.upper()),
-    format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
+    format=LOG_FORMAT,
     handlers=[
         logging.StreamHandler(sys.stdout)
-    ]
+    ],
+    force=True,
 )
 
 logger = logging.getLogger(settings.APP_NAME)
