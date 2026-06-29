e9bfe7d04c23db7ee5b71bc0b5eb41ddc276ed66
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 20:17:40 +0100

    Create prompts.py

---
+++ constants/prompts.py
@@ -0,0 +1,11 @@
+SYSTEM_PROMPT = """
+You are a helpful AI assistant.
+"""
+
+SUMMARY_PROMPT = """
+Summarize the following conversation.
+"""
+
+KNOWLEDGE_PROMPT = """
+Answer only from available knowledge.
+"""
