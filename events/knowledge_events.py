ae55c0584f0b96d1e984f0745a3f4ce90d765b38
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 20:32:40 +0100

    Create knowledge_events.py

---
+++ events/knowledge_events.py
@@ -0,0 +1,7 @@
+class KnowledgeEvents:
+
+    DOCUMENT_ADDED = "knowledge.document.added"
+
+    DOCUMENT_REMOVED = "knowledge.document.removed"
+
+    SEARCH_COMPLETED = "knowledge.search.completed"
