848839b02d15bd0ab8991bf4a290c03488a6f40f
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 20:28:36 +0100

    Create init.py

---
+++ interfaces/init.py
@@ -0,0 +1,4 @@
+from .llm_interface import LLMInterface
+from .embedding_interface import EmbeddingInterface
+from .repository_interface import RepositoryInterface
+from .cache_interface import CacheInterface
