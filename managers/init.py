2f17a7f1ab9d92056242b86d7279d5ee50f40a66
delgithub4 <103834452+delgithub4@users.noreply.github.com>
2026-06-29 20:22:01 +0100

    Create init.py

---
+++ managers/init.py
@@ -0,0 +1,5 @@
+from .conversation_manager import ConversationManager
+from .memory_manager import MemoryManager
+from .knowledge_manager import KnowledgeManager
+from .model_manager import ModelManager
+from .context_manager import ContextManager
